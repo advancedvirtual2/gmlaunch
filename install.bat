c3e7f90c2c8a1525480cd75eb215d5717a72d584
advancedvirtual2 <148706555+advancedvirtual2@users.noreply.github.com>
2024-04-01 02:27:27 +0400

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,3 @@
+rem This is a file with an installation commands that will be executed when torrent is downloaded fully.
+rem Script will be executed in the torrent root directory, so you can just run "setup.exe".
+setup.exe
